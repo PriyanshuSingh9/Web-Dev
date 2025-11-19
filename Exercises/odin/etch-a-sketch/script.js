08082996afadd9fa2d35210386d470a78a54da9a
Priyanshu Singh <priyanshu7829singh@gmail.com>
2025-11-19 18:46:44 +0530

    Etch-a-Sketch: fixed event handling situation

--- Exercises/odin/etch-a-sketch/script.js
+++ Exercises/odin/etch-a-sketch/script.js
@@ -1,19 +1,20 @@
 function createGrid(size) {
-    const container = document.querySelector('.board');
-    container.innerHTML = "";
+    const board = document.querySelector(".board");
+    board.innerHTML = "";
     for (let i = 0; i < size; i++) {
-        const row = document.createElement('div');
-        row.style.height = `${container.clientHeight / size}px`
-        row.style.width = `${container.clientWidth}px`
+        const row = document.createElement("div");
+        row.style.height = `${board.clientHeight / size}px`
+        row.style.width = `${board.clientWidth}px`
         row.classList.add('row');
         for (let j = 0; j < size; j++) {
-            const cell = document.createElement('div');
+            const cell = document.createElement("div");
             cell.classList.add('cell');
-            cell.style.height = `${container.clientHeight / size}px`;
-            cell.style.width = `${container.clientWidth / size}px`;
+            cell.style.height = `${board.clientHeight / size}px`;
+            cell.style.width = `${board.clientWidth / size}px`;
             row.appendChild(cell);
+            cell.addEventListener("mouseover", handleHover)
         }
-        container.appendChild(row);
+        board.appendChild(row);
     }
 }
 
@@ -21,38 +22,30 @@ function randomRGB() {
     return `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`
 }
 createGrid(16);
-function paintMode() {
-    let cells = document.querySelectorAll('.cell');
-    console.log("loda")
-    cells.forEach((cell) => {
-        cell.addEventListener('mouseover', () => {
-            cell.style.backgroundColor = randomRGB();
-        });
-    });
-}
-function eraseMode() {
-    let cells = document.querySelectorAll('.cell');
-    cells.forEach((cell) => {
-        cell.addEventListener('mouseover', () => {
-            cell.style.backgroundColor = "white";
-        });
-    });
+
+function handleHover(e) {
+    if (currentMode === "paint") {
+        e.target.style.backgroundColor = randomRGB();
+    } else if (currentMode === "erase") {
+        e.target.style.backgroundColor = "white";
+    }
 }
 function clearGrid() {
-    let cells = document.querySelectorAll('.cell');
+    let cells = document.querySelectorAll(".cell");
     cells.forEach(cell => {
         cell.style.backgroundColor = "white";
-        // turn off paint and erase mode
     })
 }
-let options = document.querySelector('.options');
-options.addEventListener('click', (e) => {
+
+let currentMode = "paint";
+let options = document.querySelector(".options");
+options.addEventListener("click", (e) => {
     let target = e.target.classList;
     if (target.contains("paint")) {
-        paintMode();
+        currentMode = "paint";
     }
     else if (target.contains("erase")) {
-        eraseMode();
+        currentMode = "erase";
     }
     else if (target.contains("clear")) {
         clearGrid();
@@ -62,9 +55,27 @@ options.addEventListener('click', (e) => {
     }
 });
 
-// range event listener
+
 const slider = document.querySelector("input");
-slider.addEventListener('input', (e) => {
+slider.addEventListener("input", (e) => {
     let size = e.target.value;
     createGrid(size);
 })
+// this causes leaking of events as we dont turn any event off after another option is chosen
+
+// function paintMode() {
+//     let cells = document.querySelectorAll('.cell');
+//     cells.forEach((cell) => {
+//         cell.addEventListener('mouseover', () => {
+//             cell.style.backgroundColor = randomRGB();
+//         });
+//     });
+// }
+// function eraseMode() {
+//     let cells = document.querySelectorAll('.cell');
+//     cells.forEach((cell) => {
+//         cell.addEventListener('mouseover', () => {
+//             cell.style.backgroundColor = "white";
+//         });
+//     });
+// }

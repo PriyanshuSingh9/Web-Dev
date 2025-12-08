854bcee8aa043329038f4dc5ce7e3c924d96a17c
Priyanshu Singh <priyanshu7829singh@gmail.com>
2025-12-08 06:36:16 +0530

    exoress testing post

--- cwh/node_tute/express/main.js
+++ cwh/node_tute/express/main.js
@@ -11,13 +11,19 @@ app.use(express.static('public'))
 
 // program how the app respnds when user requests home page
 app.get('/', (request, response) => {
-    response.send('Hello World')
+    console.log('this is a get request')
+    response.send('Hello World get')
 })
 
 // using request parameters to display required page in the web app
 // we can also use request queries to pass along some trivial data through the url itself
 app.get('/:slug', (req, res) => {
     res.send(`welcome to ${req.params.slug}`)
+})
+
+app.post('/table.html', (req, res) => {
+    console.log('this is a post request')
+    res.send("Hello World post")
 })
 
 // start server at chosen port

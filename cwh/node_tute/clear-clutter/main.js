3791dd8b2032eb7a776de6e081e1c12ef2e6f787
Priyanshu Singh <priyanshu7829singh@gmail.com>
2025-12-09 15:51:56 +0530

    odin: function protoype

--- cwh/node_tute/clear-clutter/main.js
+++ cwh/node_tute/clear-clutter/main.js
@@ -2,10 +2,12 @@ const fs = require('node:fs/promises');
 
 async function clean() {
 
+    // using {withFileTypes: true} gives us an array of dirent objects with important functions
     let folder = await fs.readdir('clutter', { withFileTypes: true })
     // console.log(folder)
 
     for (const file of folder) {
+        // if directory is detected no need to perform an action on it
         if (file.isDirectory()) {
             continue
         }
@@ -13,13 +15,13 @@ async function clean() {
         if (!name.includes('.')) continue;
 
         // const ext = file.split(".").at(-1)
-        const ext = file.name.split(".").at(-1)
+        const ext = name.split(".").at(-1)
         console.log(ext)
         try {
             // create folder for each extension type
             await fs.mkdir(`clutter/${ext}`, { recursive: true })
             // move files into respective directories
-            await fs.rename(`clutter/${file.name}`, `clutter/${ext}/${file.name}`)
+            await fs.rename(`clutter/${name}`, `clutter/${ext}/${name}`)
         }
         catch (err) {
             console.error('Error handling', file, err);
--- odin/adv-javascript/test.js
+++ odin/adv-javascript/test.js
@@ -4,10 +4,24 @@ function addBook(book, author) {
     }
     this.book = book
     this.author = author
-    this.print = () => {
-        console.log(this.book, this.author)
+}
+
+// we use print as a prototyppe function as we really only need one copy of it
+// i.e. no need for separate copy of function for each object
+addBook.prototype.print = () => {
+    console.log(this.book, this.author)
+}
+
+class Player {
+    constructor(name) {
+        this.name = name;
+    }
+
+    sayName() {
+        console.log(this.name);
     }
 }
+
 
 const sapiens = new addBook("Homo Sapiens", "Yuval Nova Hariri")
 sapiens.print()

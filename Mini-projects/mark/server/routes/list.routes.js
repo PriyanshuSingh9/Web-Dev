691faaaf468ff9f80f6216be1a1de49750289f03
Priyanshu Singh <priyanshu7829singh@gmail.com>
2026-01-03 22:46:50 +0530

    mark - added server entry point through server.js

--- Mini-projects/mark/server/routes/list.routes.js
+++ Mini-projects/mark/server/routes/list.routes.js
@@ -1,9 +1,10 @@
 import express from "express"
+import { addTask, deleteTask, updateTask } from "../controllers/list.controller.js";
 
 const router = express.Router()
 
 router.post("/:listId/tasks", addTask)
-
 router.delete("/:listId/tasks/:taskId", deleteTask)
-
 router.patch("/:listId/tasks/:taskId", updateTask)
+
+export default router;
---
+++ Mini-projects/mark/server/server.js
@@ -0,0 +1,24 @@
+import express from "express"
+import mongoose from "mongoose"
+import list_routes from "./routes/list.routes.js"
+
+const app = express()
+
+// middleware to parse into json
+app.use(express.json)
+
+// Any request that starts with /lists should be handled by listRoutes
+app.use("/lists", list_routes)
+
+mongoose
+    .connect("mongodb://127.0.0.1:27017/mark")
+    .then(() => {
+        console.log("MongoDB connected")
+
+        app.listen(5000, () => {
+            console.log("App is listening at port 5000")
+        })
+    })
+    .catch((err) => {
+        console.error("MongoDB connection failed", err)
+    });

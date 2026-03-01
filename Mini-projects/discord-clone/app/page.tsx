d22fbff2a46e953f04e3246580e08730a64a4de3
Priyanshu Singh <priyanshu7829singh@gmail.com>
2026-03-01 20:39:49 +0530

    discord-clone: project initialised

---
+++ Mini-projects/discord-clone/app/page.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import Navbar from "@/components/landing-page/Navbar";
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <main className="flex flex-col lg:flex-row items-center justify-center lg:justify-evenly min-h-[calc(100vh-74px)] p-8 md:p-12 gap-12 lg:gap-8">
+        <div className="flex flex-col gap-6 max-w-2xl text-center lg:text-left">
+          <h1 className="text-foreground text-5xl sm:text-6xl lg:text-7xl font-extrabold leading-tight">
+            Group Chat That's All Fun & Games
+          </h1>
+          <p className="text-muted-foreground text-base sm:text-lg md:text-xl leading-relaxed max-w-xl mx-auto lg:mx-0">
+            Discord is great for playing games and chilling with friends, or even building a worldwide community. Customize your own space to talk, play, and hang out.
+          </p>
+        </div>
+        <div className="w-full max-w-100 lg:max-w-162.5 flex justify-center">
+          <Image
+            src="/discord-home.png"
+            height={500}
+            width={650}
+            alt="landing page image"
+            className="w-full h-auto object-contain"
+            priority
+          />
+        </div>
+      </main>
+    </>
+  );
+}
---
+++ Mini-projects/discord-clone/components/landing-page/Navbar.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Image from 'next/image'
+import Link from "next/link";
+
+const Navbar = () => {
+    return (
+        <nav className='flex justify-between items-center bg-transparent max-w-7xl mx-auto w-full px-6 py-4'>
+            {/* logo */}
+            <div className='flex items-center gap-2 cursor-pointer'>
+                <Image src="/logo.svg" alt='logo' height={36} width={36}></Image>
+                <Link href="/">
+                    <span className='text-2xl sm:text-3xl text-foreground font-extrabold tracking-tight'>Discord</span>
+                </Link>
+            </div>
+
+            {/* sign in / sign up */}
+            <div className='flex items-center gap-3 md:gap-4'>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar

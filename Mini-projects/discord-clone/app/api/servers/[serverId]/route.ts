7ea6de4ff105a993feeb6722580faffa493091ce
Priyanshu Singh <priyanshu7829singh@gmail.com>
2026-03-07 19:19:36 +0530

    discord-clone: update server

---
+++ Mini-projects/discord-clone/app/api/servers/[serverId]/route.ts
@@ -0,0 +1,32 @@
+import { db } from "@/lib/db"
+import { NextResponse } from "next/server"
+import { currentUser } from "@/lib/current-user"
+
+export async function PATCH(
+    req: Request,
+    { params }: { params: { serverId: string } }
+) {
+    try {
+        const user = await currentUser()
+        if (!user) {
+            return new NextResponse("Unauthorised", { status: 401 })
+        }
+        const { serverName, imageUrl } = await req.json()
+        const serverId = params.serverId
+        const server = await db.server.update({
+            where: {
+                id: serverId,
+                userId: user.id
+            },
+            data: {
+                serverName,
+                imageUrl
+            }
+        })
+
+        return NextResponse.json(server, { status: 200 })
+    } catch (error) {
+        console.log("[SERVER_ID_PATCH]", error)
+        return new NextResponse("Internal error", { status: 500 })
+    }
+}
--- Mini-projects/discord-clone/components/server/server-header.tsx
+++ Mini-projects/discord-clone/components/server/server-header.tsx
@@ -44,7 +44,8 @@ export const ServerHeader = (
                     <DropdownMenuSeparator />
                 )}
                 {isAdmin && (
-                    <DropdownMenuItem className="px-3 py-2 text-sm cursor-pointer">
+                    <DropdownMenuItem className="px-3 py-2 text-sm cursor-pointer"
+                        onClick={() => onOpen("editServer", { server })}>
                         Server Settings
                         <Settings className="h-4 w-4 ml-auto" />
                     </DropdownMenuItem>
